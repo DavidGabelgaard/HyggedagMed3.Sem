dc1ddfb9427b41b31454e1abed65ce2c6983821b
DavidGabelgaard <davi493c@edu.easj.dk>
2021-09-06 13:50:18 +0200

    addet comments

--- src/com/company/Spitball_Java.java
+++ src/com/company/Spitball_Java.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Spitball_Java {
 
 
-    public static Boolean runthatbitch = true;
     public static String input;
     public static double TotalNumbersAddet = 0;
     public static int TimeThruLoop;
@@ -23,36 +22,34 @@ public class Spitball_Java {
         System.out.println("Input a number:");
 
 
-        while (runthatbitch) {
+        while (true) {
             //try catches errors
-
             try {
                 input = scanner.next();
+                //if the input is a q then shot down the program
                 if (input.equals("q") || input.equals("Q")) {
                     System.out.println("Have a sort of nice day");
-                    runthatbitch = false;
+                    break;
                 } else {
                     try {
+                        // add the numbers together
                         TotalNumbersAddet += Double.parseDouble(input);
+                        // a counter of how many times the while loop has run
                         TimeThruLoop++;
-                        if (TimeThruLoop != 1) {
+                        //only show the average when 2 numbers have been put in as a minimum
+                        if (TimeThruLoop > 1) {
                             System.out.println("The average is " + TotalNumbersAddet / TimeThruLoop);
                         }
-
+                    // the rey / catch message if the input is not numeric or a q
                     } catch (NumberFormatException e) {
                         System.out.println("You can only input numbers or \"q/Q\" to stop the program");
                     }
 
                     System.out.println("Input another number:");
                 }
-
-
-
-
             } catch (Exception e) {
                 System.out.println("Not a valid Input");
             }
-
 
         }
     }
